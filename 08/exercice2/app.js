f850a4d9c74dade6e84990acdb4610d3dda5ebd6
Jean-Philippe Caissy <jpcaissy@piji.ca>
2013-07-10 23:06:13 -0400

    Ajout de la route de wiki

--- 08/exercice2/app.js
+++ 08/exercice2/app.js
@@ -1,13 +1,13 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , MongoClient = require('mongodb').MongoClient
+  , wiki = require('./routes/wiki')
+  , format = require('util').format;
 
 var app = express();
 
@@ -27,8 +27,12 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/wikis', wiki.findAll);
+app.get('/wiki/:id', wiki.findById);
+app.get('/wiki/:title', wiki.findByTitle);
+app.post('/wiki', wiki.addWiki);
+app.put('/wiki/:id', wiki.updateWiki);
+app.delete('/wiki/:id', wiki.deleteWiki);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
